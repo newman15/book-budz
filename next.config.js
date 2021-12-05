69a24ffd44dfc56950447bcf4459d54659b6176e
newman15 <aaronnewman1515@gmail.com>
2021-12-05 07:43:23 -0800

    Added the env variable to connect to Database

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   
+  env: {
+    // Username = testUser, Password = 12345, Cluster = cluster0
+    URI: 'mongodb+srv://testUser:12345@cluster0.kobzn.mongodb.net?retryWrites=true&w=majority'
+  },
+
   images: {
     domains: ['books.google.com']
   }
