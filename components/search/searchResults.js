e8e464b06658ad37a466f7b34372bd0f48978366
Aaron Newman <aaronnewman@Aarons-MacBook-Air.local>
2022-01-10 06:00:53 -0800

    Temporary Session Functionality. Must sign in to search. Temporary for testing

--- components/search/searchResults.js
+++ components/search/searchResults.js
@@ -4,13 +4,17 @@ import styles from '../../styles/Card.module.css';
 import noImage from '../../public/unavailable_img.jpeg';
 import liked from '../../public/liked.svg';
 import notLiked from '../../public/not_liked.svg';
+import {useSession } from 'next-auth/react';
 
 export default function SearchResults({searchType, searchValue}) {
 
+    // Use 'useSession' to obtain the userId to pass to backend
+    const { data: session} = useSession();
+    
     // Array to store the JSX to be sent to the DOM
     let returnJSX = [];
 
-    // Itinitialize fetcher for api calls to backend
+    // Itinitialize fetcher for api calls to backend using 'useSWR'
     const fetcher = url => fetch(url).then(r => r.json());
 
     // Use the Next.js 'useSWR' hook to touch the backend API. https://swr.vercel.app/
@@ -91,8 +95,31 @@ export default function SearchResults({searchType, searchValue}) {
 
                         <div className={styles.imgNotLiked} id={'imgNotLiked' + i} 
                             onClick={() => {
+                                // If book is not liked, switch liked icon to full heart
                                 document.getElementById('imgLiked' + i).style.display = 'block';
                                 document.getElementById('imgNotLiked' + i).style.display = 'none';
+
+                                // Store book data to be sent to API route
+                                let bookData = {
+                                    userId: session.userId,
+                                    title: data[i].title,
+                                    author: data[i].author,
+                                    date: data[i].date,
+                                    isbn: data[i].isbn,
+                                    description: data[i].description,
+                                    image: data[i].image
+                                }
+
+                                // Send the book data to the backend API to be saved
+                                fetch('/api/db/saveBook', 
+                                    {
+                                        method: 'POST',
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        },
+                                        body: JSON.stringify(bookData),
+                                    }
+                                );
                             }}>
 
                             <Image 
@@ -107,8 +134,31 @@ export default function SearchResults({searchType, searchValue}) {
 
                         <div className={styles.imgLiked} id={'imgLiked' + i} 
                             onClick={() => {
+                                // If book is already liked, change liked icon to empty heart
                                 document.getElementById('imgLiked' + i).style.display = 'none';
                                 document.getElementById('imgNotLiked' + i).style.display = 'block';
+                                
+                                // Store book data to be sent to API route
+                                let bookData = {
+                                    userId: session.userId,
+                                    title: data[i].title,
+                                    author: data[i].author,
+                                    date: data[i].date,
+                                    isbn: data[i].isbn,
+                                    description: data[i].description,
+                                    image: data[i].image
+                                }
+
+                                // Send the book data to the backend API to be deleted
+                                fetch('/api/db/deleteBook', 
+                                    {
+                                        method: 'POST',
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        },
+                                        body: JSON.stringify(bookData),
+                                    }
+                                );
                             }}>
 
                             <Image 
