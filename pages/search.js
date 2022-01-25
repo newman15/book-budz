9409094a34575f51a6dd191c4d4d3c73fb334d89
Aaron Newman <aaronnewman@Aarons-MacBook-Air.local>
2022-01-25 06:35:33 -0800

    Removed sign in requirements on this page and added a prompt to inform user's they must be signed-in in order to save a book.

--- pages/search.js
+++ pages/search.js
@@ -1,29 +1,15 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession} from "next-auth/react"
 import SearchForm from "../components/search/searchForm";
 import styles from '../styles/Home.module.css';
 
 export default function Search(){
 
-    const { data: session } = useSession();
+    const { data: session} = useSession();
 
-    // If user is signed in, display the search page
-    if (session) {
-        return (
-        <>
-            <button onClick={() => signOut()}>Sign out</button>
-
-            <div className={styles.gradient}>
-                <SearchForm />
-            </div>
-        </>
-        )
-    }
-
-    // If user is not signed in, deny seach page and prompt for sign in.
     return (
-        <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-        </>
+        <div className={styles.gradient}>
+            {!session && (<h4 style={{color: "red", textAlign: 'center'}}>You must be signed in to save a book to your board!</h4>)}
+            <SearchForm />
+        </div>
     )
 }
