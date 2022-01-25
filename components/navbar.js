3dcb33f1700e45ae62d06b0f1cbfda2444407be4
Aaron Newman <aaronnewman@Aarons-MacBook-Air.local>
2022-01-25 06:30:49 -0800

    Added responsive navigation for mobile devices

--- components/navbar.js
+++ components/navbar.js
@@ -1,44 +1,96 @@
 import Link from "next/link"
 import styles from "../styles/Nav.module.css"
-import { useSession } from "next-auth/react"
+import Image from 'next/image'
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
     
     const { data: session} = useSession();
 
-    return (
+    let desktopNav = (
         <div className={styles.topNav}>
             
             <nav>
-                <ul className={styles.floatLeft}>
+                <ul className={styles.inlineListItem} id="menuItems">
                 
-                    <li className={styles.inlineListItem}>
+                    <li className={styles.floatLeft}>
                         <Link href="/">
                             <a>Home</a>
                         </Link>
                     </li>
 
-                    <li className={styles.inlineListItem}>
+                    <li className={styles.floatLeft}>
                         <Link href="/search">
                             <a>Search</a>
                         </Link>
                     </li>
 
                     {session && (
-                        <li className={styles.inlineListItem}>
+                        <li className={styles.floatLeft}>
                             {/* Do not use '<Link></Link>' in order force a page reload when clicking */}
                             <a href={`/userBoard/${encodeURI(session.userId)}`}>Book Board</a>
                         </li>
                     )}
 
-                    
+                    {!session && (<li className={styles.floatRight}><span><button className={styles.button} onClick={() => signIn()}>Sign in</button></span></li>)}
+
+                    {session && (<li className={styles.floatRight}><span><button className={styles.button} onClick={() => signOut()}>Sign out</button></span></li>)}
+
                 </ul>
-
-                {!session && (<span className={styles.floatRight}>You are not signed in</span>)}
-
-                {session && (<span className={styles.floatRight}>You are signed in as {session.user.email} </span>)}
+                
             </nav>
             
         </div>
+    )
+
+    let mobileNav = (
+        <div className={styles.mobileTopNav}>
+            
+            <nav>
+                <ul className={styles.inlineListItem} id="menuItems">
+
+                    <div>
+                        <li className={styles.menuBarExpanded}>
+                            <Link href="/">
+                                <a>Home</a>
+                            </Link>
+                        </li>
+
+                        <li className={styles.menuBarExpanded}>
+                            <Link href="/search">
+                                <a>Search</a>
+                            </Link>
+                        </li>
+
+                        {session && (
+                            <li className={styles.menuBarExpanded}>
+                                {/* Do not use '<Link></Link>' in order force a page reload when clicking */}
+                                <a href={`/userBoard/${encodeURI(session.userId)}`}>Book Board</a>
+                            </li>
+                        )}
+
+                        {!session && (<li className={styles.menuBarExpanded}><span><button className={styles.button} onClick={() => signIn()}>Sign in</button></span></li>)}
+
+                        {session && (<li className={styles.menuBarExpanded}><span><button className={styles.button} onClick={() => signOut()}>Sign out</button></span></li>)}
+                    </div>
+                
+                </ul>
+                
+            </nav>
+        
+        </div>
+    )
+
+    return (
+        <div>
+
+            <div>
+                {desktopNav}
+                {mobileNav}
+            </div>
+            
+        </div>
+        
     )
 }
