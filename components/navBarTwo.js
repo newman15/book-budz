56b54a17128909b63bbb53d17e3977050ac362a3
Aaron Newman <aaronnewman@Aarons-MacBook-Air.local>
2022-01-25 06:31:42 -0800

    A test file for responsive navigation. Keep ideas without altering main file.

---
+++ components/navBarTwo.js
@@ -0,0 +1,112 @@
+import Link from "next/link"
+import styles from "../styles/Nav.module.css"
+import Image from 'next/image'
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useEffect, useState } from "react";
+
+export default function Navbar() {
+    
+    const { data: session} = useSession();
+    const {navState, setNavState} = useState(false);
+
+    useEffect(() => {
+        if (navState === false){
+            document.getElementById("mobileNavElements").style.display = "none";
+        }
+        else {
+            document.getElementById("mobileNavElements").style.display = "block";
+        }
+    })
+
+    let desktopNav = (
+        <div className={styles.topNav}>
+            
+            <nav>
+                <ul className={styles.inlineListItem} id="menuItems">
+                
+                    <li className={styles.floatLeft}>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+
+                    <li className={styles.floatLeft}>
+                        <Link href="/search">
+                            <a>Search</a>
+                        </Link>
+                    </li>
+
+                    {session && (
+                        <li className={styles.floatLeft}>
+                            {/* Do not use '<Link></Link>' in order force a page reload when clicking */}
+                            <a href={`/userBoard/${encodeURI(session.userId)}`}>Book Board</a>
+                        </li>
+                    )}
+
+                    {!session && (<li className={styles.floatRight}><span><button className={styles.button} onClick={() => signIn()}>Sign in</button></span></li>)}
+
+                    {session && (<li className={styles.floatRight}><span><button className={styles.button} onClick={() => signOut()}>Sign out</button></span></li>)}
+
+                </ul>
+                
+            </nav>
+            
+        </div>
+    )
+
+    let mobileNav = (
+        <div className={styles.mobileTopNav}>
+            
+            <nav>
+                <ul className={styles.inlineListItem} id="menuItems">
+
+                    <li>
+                        <button onClick={() => setNavState(!navState)}><Image src="/icons/menuBars.svg" alt="Github Logo" width={40} height={18} /></button>
+                    </li>
+
+                    <div id="mobileNavElements" style={{display: 'none'}}>
+                        <li className={styles.menuBarExpanded}>
+                            <Link href="/">
+                                <a>Home</a>
+                            </Link>
+                        </li>
+
+                        <li className={styles.menuBarExpanded}>
+                            <Link href="/search">
+                                <a>Search</a>
+                            </Link>
+                        </li>
+
+                        {session && (
+                            <li className={styles.menuBarExpanded}>
+                                {/* Do not use '<Link></Link>' in order force a page reload when clicking */}
+                                <a href={`/userBoard/${encodeURI(session.userId)}`}>Book Board</a>
+                            </li>
+                        )}
+
+                        {!session && (<li className={styles.menuBarExpanded}><span><button className={styles.button} onClick={() => signIn()}>Sign in</button></span></li>)}
+
+                        {session && (<li className={styles.menuBarExpanded}><span><button className={styles.button} onClick={() => signOut()}>Sign out</button></span></li>)}
+                    </div>
+                
+                    
+
+                </ul>
+                
+            </nav>
+        
+        </div>
+    )
+
+    return (
+        <div>
+
+            <div>
+                {desktopNav}
+                {mobileNav}
+            </div>
+            
+        </div>
+        
+    )
+}
